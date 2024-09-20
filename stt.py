134dbdbf8ecfc416c83b2380a68f57b6dba557b4
GilGeva1 <gevagilgeva@gmail.com>
2024-09-20 13:06:41 +0300

    stt

---
+++ stt.py
@@ -0,0 +1,17 @@
+import openai
+import os
+openai.api_key = os.getenv("OPENAI_API_KEY")
+audio_path = '/Users/gilgeva/Downloads/speakcare-demo-main/Taking_Blood_Sample.mp3'
+
+def transcribe_audio(audio_path):
+    with open(audio_path, "rb") as audio_file:
+        transcript = openai.Audio.transcribe(
+            model="whisper-1", 
+            file=audio_file
+        )
+    return transcript['text']
+
+if __name__ == "__main__":
+
+    transcription = transcribe_audio(audio_path)
+    print(transcription)
