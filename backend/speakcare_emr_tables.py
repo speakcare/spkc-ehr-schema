42c30f336c47c0b6de0e1a86064e22056457d3dd
orifinkelman <orifinkleman@gmail.com>
2025-05-06 23:33:46 -0400

    add MHCS.eINTERACT_Change_in_Condition_Evaluation

--- backend/speakcare_emr_tables.py
+++ backend/speakcare_emr_tables.py
@@ -84,6 +84,7 @@ class SpeakCareEmrTables:
     GROVE_ASSESSMENT_DETAILS_TABLE = 'Grove_Assessment_Details'
     MHCS_NURSING_DAILY_SKILLED_NOTE_TABLE = 'MHCS.Nursing_Daily_Skilled_Note'
     MHCS_ADMISSION_DETAILS_TABLE = 'MHCS.Admission_Details'
+    MHCS_EINTERACT_EVALUATION_TABLE = 'MHCS.eINTERACT_Change_in_Condition_Evaluation'
     
     
 
@@ -128,7 +129,7 @@ class SpeakCareEmrTables:
         GROVE_ASSESSMENT_DETAILS_TABLE,
         MHCS_NURSING_DAILY_SKILLED_NOTE_TABLE,
         MHCS_ADMISSION_DETAILS_TABLE,
-        
+        MHCS_EINTERACT_EVALUATION_TABLE,
         # WEIGHTS_TABLE, 
         # BLOOD_PRESSURES_TABLE, 
         # BLOOD_SUGARS_TABLE, 
